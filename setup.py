aacb89e7f6e8084aa29f92feeeb6661463acf4d1
Colm Britton <colmjude@gmail.com>
2020-09-25 11:51:11 +0100

    basic setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="digital-land-frontend",
+    version="0.1.0",
+    author="Digital land",
+    description="Reusable frontend code for digital land services and products",
+    license="MIT",
+    packages=["digital-land-frontend"],
+    package_data={'digital-land-frontend': ['templates/**.*']},
+    python_requires=">=3.5",
+    install_requires=[
+        "jinja2",
+    ],
+)
