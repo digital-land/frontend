137c8ba86c014ed4c811f8b4758d6f8cc0bce9ba
Colm Britton <colmjude@gmail.com>
2020-10-14 14:03:26 +0100

    bump version

--- setup.py
+++ setup.py
@@ -8,19 +8,24 @@ directories_html = glob.glob("digital_land_frontend/**/**/*.html", recursive=Tru
 directories_jinja = glob.glob("digital_land_frontend/**/**/*.jinja", recursive=True)
 
 for directory in directories_html:
-    components.append(os.path.relpath(os.path.dirname(directory), "digital_land_frontend") + "/*.html")
+    components.append(
+        os.path.relpath(os.path.dirname(directory), "digital_land_frontend") + "/*.html"
+    )
 
 for directory in directories_jinja:
-    components.append(os.path.relpath(os.path.dirname(directory), "digital_land_frontend") + "/*.jinja")
+    components.append(
+        os.path.relpath(os.path.dirname(directory), "digital_land_frontend")
+        + "/*.jinja"
+    )
 
 setup(
     name="digital-land-frontend",
-    version="0.1.6",
+    version="0.1.7",
     author="Digital land",
     description="Reusable frontend code for digital land services and products",
     license="MIT",
     packages=["digital_land_frontend"],
-    package_data={'digital-land-frontend': components},
+    package_data={"digital-land-frontend": components},
     python_requires=">=3.5",
     install_requires=[
         "jinja2",
