f9b03720c7808c63b5f480237ea870135c3373b1
Colm Britton <colmjude@gmail.com>
2020-10-14 15:46:27 +0100

    Add a filter to convert urls to anchors

--- digital_land_frontend/filters.py
+++ digital_land_frontend/filters.py
@@ -1,4 +1,6 @@
 import os
+import validators
+from jinja2 import evalcontextfilter, Markup
 
 
 def get_jinja_template_raw(template_file_path):
@@ -7,3 +9,17 @@ def get_jinja_template_raw(template_file_path):
             file = open(template_file_path, "r")
             return file.read()
     return None
+
+
+@evalcontextfilter
+def make_link(eval_ctx, url):
+    """
+    Converts a url string into an anchor element.
+
+    Requires autoescaping option to be set to True
+    """
+    if validators.url(url):
+        anchor = f'<a class="govuk-link" href="{url}">{url}</a>'
+        if eval_ctx.autoescape:
+            return Markup(anchor)
+    return url
