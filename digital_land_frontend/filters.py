008e5c85960f1f7efa1bdf27e233c17c26c8488f
Colm Britton <colmjude@gmail.com>
2020-10-29 15:55:58 +0000

    Add some commonly used jinja filters e.g. commanum

--- digital_land_frontend/filters.py
+++ digital_land_frontend/filters.py
@@ -24,3 +24,27 @@ def make_link(eval_ctx, url):
             if eval_ctx.autoescape:
                 return Markup(anchor)
     return url
+
+
+def is_valid_uri(uri):
+    """
+    Checks if a string is valid URI
+    """
+    if validators.url(uri):
+        return True
+    return False
+
+
+def float_to_int(v):
+    if v:
+        return int(float(v))
+    return ""
+
+
+def commanum(v): 
+    """
+    Makes large numbers readable by adding commas
+
+    E.g. 1000000 -> 1,000,000
+    """
+    return "{:,}".format(v)
