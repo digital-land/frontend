5e7e8918eb46a9110cc58481ee9df59625dac7a9
Colm Britton <colmjude@gmail.com>
2020-11-27 15:37:41 +0000

    Add curie to url part method to utils

--- src/js/helpers/utils.js
+++ src/js/helpers/utils.js
@@ -4,6 +4,10 @@ function camelCaseReplacer (match, s) {
   return s.toUpperCase()
 }
 
+utils.curie_to_url_part = function (curie) {
+  return curie.replace(':', '/')
+}
+
 utils.toCamelCase = function (s) {
   return s.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, camelCaseReplacer)
 }
