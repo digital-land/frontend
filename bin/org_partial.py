b0861df438497f29f4eb2c9dabc04e4586f87421
Colm Britton <colmjude@gmail.com>
2020-11-30 18:51:13 +0000

    Generate an organisation autocomplete partial

---
+++ bin/org_partial.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import sys
+import jinja2
+
+# add parent directory
+sys.path.append(".")
+
+from digital_land_frontend.jinja_filters.organisation_mapper import (
+    OrganisationMapper,
+)
+
+
+def setup_jinja():
+    # register templates
+    multi_loader = jinja2.ChoiceLoader(
+        [
+            jinja2.FileSystemLoader(searchpath="./src/templates"),
+            jinja2.PrefixLoader(
+                {
+                    "digital-land-frontend": jinja2.FileSystemLoader(
+                        searchpath="./digital_land_frontend/templates"
+                    ),
+                    "govuk-jinja-components": jinja2.PackageLoader(
+                        "govuk_jinja_components"
+                    ),
+                }
+            ),
+        ]
+    )
+    env = jinja2.Environment(loader=multi_loader)
+
+    return env
+
+
+env = setup_jinja()
+organisation_autocomplete_template = env.get_template("organisation-autocomplete.html")
+
+# data for organisation autocomplete
+organisation_mapper = OrganisationMapper()
+orgs = [{"value": k, "text": v} for k, v in organisation_mapper.all().items()]
+
+
+partials_dir = "digital_land_frontend/templates/partials"
+with open(f"{partials_dir}/dl-organisation-autocomplete.html", "w") as f:
+    f.write(organisation_autocomplete_template.render(orgs=orgs))
