0aeacfb2cd7189f25c05006d078890ce05a40ac4
Colm Britton <colmjude@gmail.com>
2020-12-08 16:28:03 +0000

    Add func to create organisation mapper

--- src/js/helpers/utils.js
+++ src/js/helpers/utils.js
@@ -16,4 +16,16 @@ utils.truncate = function (s, len) {
   return s.slice(0, len) + '...'
 }
 
+/**
+ * Create an organisation mapper. Maps organisation ids to names
+ * @param  {Array} orgsObj Array of organisation objs. Must contain .id and .name propterties
+ */
+utils.createOrgMapper = function (orgsObj) {
+  const mapper = {}
+  orgsObj.forEach(function (o) {
+    mapper[o.id] = o.name
+  })
+  return mapper
+}
+
 export default utils
