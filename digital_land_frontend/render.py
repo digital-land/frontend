d6a429487afd874cec7a78878361fb8f99042098
Jon Bramley <brammers@gmail.com>
2020-12-09 15:33:22 +0000

    feat: migrate to pathlib and support geometry field

--- digital_land_frontend/render.py
+++ digital_land_frontend/render.py
@@ -1,18 +1,19 @@
 import csv
 import json
 import logging
-import os
 from collections import OrderedDict
+from pathlib import Path
 
 import shapely.wkt
 
 from digital_land_frontend.jinja import setup_jinja
-from digital_land_frontend.jinja_filters.organisation_mapper import \
-    OrganisationMapper
+from digital_land_frontend.jinja_filters.organisation_mapper import OrganisationMapper
 
 
 class Renderer:
     organisation_mapper = OrganisationMapper()
+    translations = str.maketrans({"/": "-", " ": "", "(": "", ")": "", "'": ""})
+    geometry_fields = ["geometry", "point"]
 
     def __init__(self, name, dataset, url_root=None, docs="docs"):
         self.name = name
@@ -27,8 +28,6 @@ class Renderer:
             self.env.globals["urlRoot"] = url_root
         else:
             self.env.globals["urlRoot"] = f"/{name.replace(' ', '-')}/"
-
-    translations = str.maketrans({"/": "-", " ": "", "(": "", ")": "", "'": ""})
 
     def get_id(self, row, idx):
         id = row["site"].translate(self.translations)
@@ -64,9 +63,17 @@ class Renderer:
         rows = []
         for idx, row in enumerate(csv.DictReader(open(self.dataset)), start=1):
             row["id"] = self.get_id(row, idx)
-            self.create_geometry_file(row)
+            output_dir = Path(self.docs, row["id"])
+            if not output_dir.exists():
+                output_dir.mkdir()
+
+            for field in self.geometry_fields:
+                if field in row and row[field]:
+                    self.create_geometry_file(output_dir, row, field)
+                    break
+
             self.render(
-                f"{row['id']}/index.html",
+                output_dir / "index.html",
                 self.row_template,
                 row=row,
                 data_type=self.name,
@@ -81,29 +88,23 @@ class Renderer:
         self.render("index.html", self.index_template, index=index, data_type=self.name)
 
     def render(self, path, template, **kwargs):
-        path = os.path.join(self.docs, path)
-        directory = os.path.dirname(path)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
         with open(path, "w") as f:
             logging.debug(f"creating {path}")
             f.write(template.render(**kwargs))
 
-    def create_geometry_file(self, area):
-        area_dir = f"{self.docs}/{area['id']}"
-        if area["point"] == "POINT( )":
+    def create_geometry_file(self, output_dir, row, field):
+        # Remove this once dataset is fixed
+        if row[field] == "POINT( )":
             return
-        if not os.path.exists(area_dir):
-            os.mkdir(area_dir)
+
         try:
             geojson = {"type": "Feature"}
-            geojson["geometry"] = wkt_to_json_geometry(area["point"])
-            geojson["properties"] = area
-            with open(f"{area_dir}/geometry.geojson", "w") as f:
+            geojson["geometry"] = wkt_to_json_geometry(row[field])
+            geojson["properties"] = row
+            with open(output_dir / "geometry.geojson", "w") as f:
                 json.dump(geojson, f)
         except Exception as e:
-            print(e)
+            logging.exception(e)
 
 
 def wkt_to_json_geometry(input_):
