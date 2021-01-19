b12c2f06873cb7fa1999146e21a993157ac4b1bf
Colm Britton <colmjude@gmail.com>
2021-01-19 11:33:31 +0000

    Add table extension to markdown setup

--- digital_land_frontend/markdown/filter.py
+++ digital_land_frontend/markdown/filter.py
@@ -6,11 +6,13 @@ from digital_land_frontend.markdown.govukify import govukify
 from markdown.extensions.toc import TocExtension
 
 # init markdown - basic setup with table of contents
-md = markdown.Markdown(extensions=[TocExtension(toc_depth="2-3")])
+md = markdown.Markdown(extensions=[TocExtension(toc_depth="2-3"), "tables"])
+
 
 def compile_markdown(s, md=md):
     html = md.convert(s)
     return govukify(html)
+
 
 # making markdown compiler available to jinja templates
 def markdown_filter(s):
