0415b937248cee126e94d4516149e5fbf1b98bd1
Colm Britton <colmjude@gmail.com>
2021-02-03 13:28:18 +0000

    basic popup for polygon features

--- src/js/dl-maps.js
+++ src/js/dl-maps.js
@@ -1,7 +1,9 @@
 import Map from './dl-maps/map'
 import brownfieldSites from './dl-maps/brownfield-land-sites'
+import basicPopup from './dl-maps/basic-popup'
 
 export {
   Map,
-  brownfieldSites
+  brownfieldSites,
+  basicPopup
 }
