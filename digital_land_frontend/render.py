835defb2bce7a506de28149e8a1c2c4822442392
Jon Bramley <brammers@gmail.com>
2021-02-10 18:55:30 +0000

    feat: tests for renderer and some refactoring

--- digital_land_frontend/render.py
+++ digital_land_frontend/render.py
@@ -13,6 +13,44 @@ from digital_land_frontend.jinja_filters.mappers import (
     OrganisationMapper,
 )
 
+# TODO:
+#   - add group_field to specification
+#   - remove `references` from render parameters - use "seen" sets
+#   - clean up template logic now that we always provide href, text and reference
+
+
+class JinjaRenderer:
+    def __init__(self, url_root, docs="docs"):
+        self.docs = docs
+        self.env = setup_jinja()
+        self.env.globals["urlRoot"] = url_root
+        self.env.trim_blocks = True
+        self.env.lstrip_blocks = True
+        self.template = {
+            "index": self.env.get_template("index.html"),
+            "row": self.env.get_template("row.html"),
+        }
+
+    def render_index(self, path, *args, **kwargs):
+        JinjaRenderer._render(
+            path,
+            self.template["index"],
+            **kwargs,
+        )
+
+    def render_row(self, path, **kwargs):
+        JinjaRenderer._render(
+            path,
+            self.template["row"],
+            **kwargs,
+        )
+
+    @staticmethod
+    def _render(path, template, **kwargs):
+        with open(path, "w") as f:
+            logging.debug(f"creating {path}")
+            f.write(template.render(**kwargs))
+
 
 class Renderer:
     organisation_mapper = OrganisationMapper()
@@ -23,66 +61,58 @@ class Renderer:
     def __init__(
         self,
         name,
-        schema,
-        dataset_path,
         url_root=None,
         key_fields=["organisation", "site"],
-        group_type="organisation",
         group_field="organisation",
         group_list_field="organisations",
         docs="docs",
+        renderer=None,
     ):
         self.name = name
-        self.schema = schema
-        self.dataset_path = dataset_path
         self.docs = Path(docs)
         self.key_fields = key_fields
-        self.group_type = group_type
         self.group_field = group_field
         self.group_list_field = group_list_field
-        self.env = setup_jinja()
         self.index = defaultdict(lambda: {"count": 0, "references": set(), "items": []})
-        self.index_template = self.env.get_template("index.html")
-        self.row_template = self.env.get_template("row.html")
         self.group_map = {}
         self.group_slug_seen = set()
         self.slug_seen = set()
 
-        if url_root:
-            self.env.globals["urlRoot"] = url_root
-        else:
-            self.env.globals["urlRoot"] = f"/{name.replace(' ', '-')}/"
+        if not url_root:
+            url_root = f"/{name.replace(' ', '-')}/"
+
+        self.renderer = renderer or JinjaRenderer(url_root, docs)
 
     def add_to_group_index(self, row):
-        if self.group_type and self.group_field in row and row[self.group_field]:
+        if self.group_field and self.group_field in row and row[self.group_field]:
             groups = [row[self.group_field]]
         elif (
-            self.group_type
+            self.group_field
             and self.group_list_field in row
             and row[self.group_list_field]
         ):
             groups = row[self.group_list_field].split(";")
         else:
-            groups = ["no-group"]
+            groups = [None]
 
         for group in groups:
             self.add_row_to_group_map(group, row)
 
     @property
     def group_index(self):
-        for org, idx in self.group_map.items():
-            self.group_map[org]["items"] = sorted(
-                idx["items"], key=lambda x: AlphaNumericSort.alphanum(x["slug"])
+        for group, idx in self.group_map.items():
+            self.group_map[group]["items"] = sorted(
+                idx["items"], key=lambda x: AlphaNumericSort.alphanum(x["reference"])
             )
 
         result = OrderedDict(
-            sorted(self.group_map.items(), key=lambda x: x[1]["name"] or "")
+            sorted(self.group_map.items(), key=lambda x: x[1]["text"] or "")
         )
 
-        if "no-group" in result:
-            result.move_to_end("no-group")
-
-        return result
+        if self.group_field:
+            return result
+        else:
+            return result[None]["items"]
 
     def add_row_to_group_map(self, group, row):
         self.slug_seen.add(row["slug"])
@@ -90,28 +120,34 @@ class Renderer:
         if dupe_check_key in self.group_slug_seen:
             return
 
-        if self.group_type is None:
+        if self.group_field is None:
 
             def name_map_func(name):
                 return name
 
-        elif self.group_type == "organisation":
+        elif self.group_field == "organisation":
             name_map_func = self.organisation_mapper.get_by_key
         else:
-            raise NotImplementedError("group_type %s not supported" % self.group_type)
+            raise NotImplementedError("group_field %s not supported" % self.group_field)
 
         o = {
-            "name": name_map_func(group),
+            "text": name_map_func(group) or group,
             "items": [],
         }
         self.group_map.setdefault(group, o)
-        self.group_map[group]["items"].append(row)
+        reference = row["slug"].split("/")[-1]
+        self.group_map[group]["items"].append(
+            self.index_entry(reference, row["name"], slug=row["slug"])
+        )
         self.group_slug_seen.add(dupe_check_key)
 
-    def render_pages(self):
+    def render_dataset(self, dataset_path):
+        self.render(csv.DictReader(open(dataset_path)))
+
+    def render(self, reader):
         self.slugs = set()
         rows = []
-        for idx, row in enumerate(csv.DictReader(open(self.dataset_path)), start=1):
+        for idx, row in enumerate(reader, start=1):
             if not row["slug"]:
                 continue  # skip rows without a unique slug
 
@@ -122,11 +158,9 @@ class Renderer:
 
             breadcrumb = slug_to_breadcrumb(row["slug"])
 
-            row["href"] = "/".join(
-                row["slug"].split("/")[2:]
-            )  # strip the prefix from slug
+            path = "/".join(row["slug"].split("/")[2:])  # strip the prefix from slug
 
-            output_dir = self.docs / row["href"]
+            output_dir = self.docs / path
             if not output_dir.exists():
                 output_dir.mkdir(parents=True)
 
@@ -136,24 +170,26 @@ class Renderer:
                     row["geometry_url"] = "geometry.geojson"
                     break
 
-            self.render(
-                output_dir / "index.html",
-                self.row_template,
+            self.renderer.render_row(
+                str(output_dir / "index.html"),
                 row=row,
                 data_type=self.name,
-                schema=self.schema,
                 breadcrumb=breadcrumb,
             )
             rows.append(row)
             self.add_to_index(row["slug"], row)
             self.slugs.add(row["slug"])
 
-        self.index[""] = {
-            "count": len(self.slug_seen),
-            "groups": self.group_index,
-            "group_type": self.group_type,
-        }
+        root_index = {"data_type": self.name}
+        if self.group_field:
+            root_index["groups"] = self.group_index
+        else:
+            root_index["items"] = self.group_index
 
+        root_index["group_field"] = self.group_field
+        root_index["count"] = len(self.slug_seen)
+
+        self.index[""] = root_index
         self.render_index_pages()
 
     def add_to_index(self, slug, row):
@@ -166,25 +202,31 @@ class Renderer:
             return
 
         stem, name = slug.rsplit("/", 1)
-        self.index[stem].setdefault(
-            "groups", {"no-group": {"items": [], "references": set()}}
-        )
+        self.index[stem].setdefault("items", [])
+        self.index[stem].setdefault("references", set())
         self.index[stem].setdefault("count", 0)
-        self.index[stem].setdefault("group_type", None)
+        self.index[stem].setdefault("group_field", None)
 
-        if name in self.index[stem]["groups"]["no-group"]["references"]:
+        if name in self.index[stem]["references"]:
             return
 
-        self.index[stem]["groups"]["no-group"]["references"].add(name)
+        self.index[stem]["references"].add(name)
         self.index[stem]["count"] += 1
-        index_entry = {
-            "reference": format_name(name) if not row else name,
-            "href": slug_to_relative_path(slug, strip_prefix=stem),
-        }
-        if row:
-            index_entry["text"] = row["name"]
-        self.index[stem]["groups"]["no-group"]["items"].append(index_entry)
+
+        reference = format_name(name) if not row else name
+        href = slug_to_relative_href(slug, strip_prefix=stem)
+        text = row["name"] if row else None
+
+        self.index[stem]["items"].append(self.index_entry(reference, text, href))
         self._add_to_index(stem)
+
+    def index_entry(self, reference, text, href=None, slug=None):
+        if href:
+            return {"reference": reference, "text": text, "href": href}
+        elif slug:
+            return {"reference": reference, "text": text, "slug": slug}
+        else:
+            raise ValueError("index entry requires href or slug")
 
     def render_index_pages(self):
         for path, i in self.index.items():
@@ -193,27 +235,33 @@ class Renderer:
                 download_url = None
             else:
                 slug = f"/{self.name}"
-                download_url = f"https://raw.githubusercontent.com/digital-land/permitted-development-right/main/{self.dataset_path}"
+                download_url = f"https://raw.githubusercontent.com/digital-land/{self.name}/main/dataset/{self.name}.csv"
             if "items" in i:
+                for item in i["items"]:
+                    if "slug" in item:
+                        item["href"] = slug_to_relative_href(
+                            item["slug"], "/".join([self.name, path])
+                        )
+                        del item["slug"]
                 i["items"] = sorted(
                     i["items"], key=lambda x: AlphaNumericSort.alphanum(x["reference"])
                 )
+
+            for group in i.get("groups", {}).values():
+                for item in group.get("items", []):
+                    if "slug" in item:
+                        item["href"] = slug_to_relative_href(
+                            item["slug"], "/".join([self.name, path])
+                        )
+                        del item["slug"]
+
             logging.debug("rendering %s", path)
-            self.render(
-                self.docs / path / "index.html",
-                self.index_template,
-                index=i,
-                data_type=self.name,
-                schema=self.schema,
+            self.renderer.render_index(
+                str(self.docs / path / "index.html"),
+                **i,
                 breadcrumb=slug_to_breadcrumb(slug),
                 download_url=download_url,
             )
-
-    @staticmethod
-    def render(path, template, **kwargs):
-        with open(path, "w") as f:
-            logging.debug(f"creating {path}")
-            f.write(template.render(**kwargs))
 
 
 re_all_upper = re.compile(r"^[A-Z]*$")
@@ -251,13 +299,15 @@ def slug_to_breadcrumb(slug):
     return breadcrumb
 
 
-def slug_to_relative_path(slug, strip_prefix=None):
-    logging.debug(">> slug_to_relative_path(%s, %s)", slug, strip_prefix)
+def slug_to_relative_href(slug, strip_prefix=None):
+    logging.debug(">> slug_to_relative_href(%s, %s)", slug, strip_prefix)
     if slug.startswith("/"):
         slug = slug[1:]
 
     if strip_prefix and slug.startswith(strip_prefix):
-        slug = slug[len(strip_prefix) + 1 :]
+        slug = slug[len(strip_prefix) :]
+        if slug.startswith("/"):
+            slug = slug[1:]
 
     logging.debug("<< " + str(strip_prefix) + "   ./" + slug)
     return "./" + slug
