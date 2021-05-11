10ec0e0f7192309e860db146a24b4aa8358eacc1
Colm Britton <colmjude@gmail.com>
2021-05-11 13:33:00 +0100

    Add filter to count total number of references

--- digital_land_frontend/filters.py
+++ digital_land_frontend/filters.py
@@ -290,3 +290,8 @@ def key_field_filter(record, pipeline_name):
 
 def github_line_num_filter(n):
     return str(int(n) + 1)
+
+
+def total_items_filter(obj):
+    counts = [len(v) for k, v in obj.items()]
+    return sum(counts)
