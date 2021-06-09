bb5ebf00609ca4a20a9a0bc3b302121ac1e38fb0
Colm Britton <colmjude@gmail.com>
2021-06-09 17:03:44 +0100

    latest build of javascripts

--- digital_land_frontend/static/javascripts/dl-maps.js
+++ digital_land_frontend/static/javascripts/dl-maps.js
@@ -574,7 +574,292 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
   var basicPopup = {
     initOnEachFeature: initOnEachFeature
   };
+  /* global L, window, DLMaps */
+
+  function LayerControls($module, leafletMap) {
+    this.$module = $module;
+    this.map = leafletMap;
+  }
+
+  LayerControls.prototype.init = function (params) {
+    this.setupOptions(params); // returns a node list so convert to array
+
+    var $controls = this.$module.querySelectorAll(this.layerControlSelector);
+    this.$controls = Array.prototype.slice.call($controls);
+    this.datasetNames = this.$controls.map(function ($control) {
+      return $control.dataset.layerControl;
+    }); // setup default options for geojsonFeatureLayers
+
+    var boundGetLayerStyleOption = this.getLayerStyleOption.bind(this);
+    var boundOnEachFeature = this.onEachFeature.bind(this);
+    this.geoJsonLayerOptions = {
+      style: boundGetLayerStyleOption,
+      onEachFeature: boundOnEachFeature
+    }; // create mapping between dataset and layer, one per control item
+
+    this.layerMap = this.createAllFeatureLayers(); // listen for changes to URL
+
+    var boundSetControls = this.setControls.bind(this);
+    window.addEventListener('popstate', function (event) {
+      console.log('URL has changed - back button');
+      boundSetControls();
+    }); // initial set up of controls (default or urlParams)
+
+    var urlParams = new URL(document.location).searchParams;
+
+    if (!urlParams.has('layer')) {
+      // if not set then use default checked controls
+      this.updateURL();
+    } else {
+      // use URL params if available
+      this.setControls();
+    } // listen for changes on each checkbox
+
+
+    var boundControlChkbxChangeHandler = this.onControlChkbxChange.bind(this);
+    this.$controls.forEach(function ($control) {
+      console.log(this);
+      $control.addEventListener('change', boundControlChkbxChangeHandler, true);
+    }, this);
+    return this;
+  };
+
+  LayerControls.prototype.onControlChkbxChange = function (e) {
+    console.log("I've been toggled", e.target, this); // get the control containing changed checkbox
+
+    var $clickedControl = e.target.closest(this.layerControlSelector); // when a control is changed update the URL params
+
+    this.updateURL(); // run provided callback
+
+    var enabled = this.getCheckbox($clickedControl).checked;
+
+    if (this.toggleControlCallback && utils.isFunction(this.toggleControlCallback)) {
+      this.toggleControlCallback(this.map, this.getDatasetName($clickedControl), enabled);
+    }
+  }; // should this return an array or a single control?
+
+
+  LayerControls.prototype.getControlByName = function (dataset) {
+    for (var i = 0; i < this.$controls.length; i++) {
+      var $control = this.$controls[i];
+
+      if ($control.dataset.layerControl === dataset) {
+        return $control;
+      }
+    }
+
+    return undefined;
+  };
+
+  LayerControls.prototype.createFeatureLayer = function () {
+    return L.geoJSON(false, this.geoJsonLayerOptions).addTo(this.map);
+  };
+
+  LayerControls.prototype.createAllFeatureLayers = function () {
+    var layerToDatasetMap = {};
+    var that = this;
+    this.$controls.forEach(function ($control) {
+      var dataset = that.getDatasetName($control);
+      var layer;
+
+      if (dataset === 'brownfield-land') {
+        layer = DLMaps.brownfieldSites.geojsonToLayer(false, that.geoJsonLayerOptions).addTo(that.map);
+      } else {
+        layer = that.createFeatureLayer();
+      }
+
+      layerToDatasetMap[dataset] = layer;
+    });
+    return layerToDatasetMap;
+  };
+
+  LayerControls.prototype.getLayerStyleOption = function (feature) {
+    var colour = this.getStyle(this.getControlByName(feature.properties.type));
+
+    if (typeof colour === 'undefined') {
+      return {
+        color: '#003078',
+        weight: 2
+      };
+    } else {
+      return {
+        color: colour,
+        weight: 2
+      };
+    }
+  };
+
+  LayerControls.prototype.enable = function ($control) {
+    console.log('enable', this.getDatasetName($control));
+    var $chkbx = $control.querySelector('input[type="checkbox"]');
+    $chkbx.checked = true;
+    $control.dataset.layerControlActive = 'true';
+    $control.classList.remove(this.layerControlDeactivatedClass);
+  };
+
+  LayerControls.prototype.disable = function ($control) {
+    console.log('disable', this.getDatasetName($control));
+    var $chkbx = $control.querySelector('input[type="checkbox"]');
+    $chkbx.checked = false;
+    $control.dataset.layerControlActive = 'false';
+    $control.classList.add(this.layerControlDeactivatedClass);
+  };
+
+  LayerControls.prototype.setControls = function () {
+    var _this2 = this;
+
+    var urlParams = new URL(document.location).searchParams;
+
+    if (urlParams.has('layer')) {
+      // get the names of the enabled and disabled layers
+      // only care about layers that exist
+      var enabledLayerNames = urlParams.getAll('layer').filter(function (name) {
+        return _this2.datasetNames.indexOf(name) > -1;
+      });
+      console.log('Enable:', enabledLayerNames);
+      var datasetNamesClone = [].concat(this.datasetNames);
+      var disabledLayerNames = datasetNamesClone.filter(function (name) {
+        return enabledLayerNames.indexOf(name) === -1;
+      }); // map the names to the controls
+
+      var toEnable = enabledLayerNames.map(function (name) {
+        return _this2.getControlByName(name);
+      });
+      var toDisable = disabledLayerNames.map(function (name) {
+        return _this2.getControlByName(name);
+      });
+      console.log(toEnable, toDisable); // pass correct this arg
+
+      toEnable.forEach(this.enable, this);
+      toDisable.forEach(this.disable, this);
+    }
+  };
+
+  LayerControls.prototype.updateURL = function () {
+    var _this3 = this;
+
+    var urlParams = new URL(document.location).searchParams;
+    var enabledLayers = this.enabledLayers().map(function ($control) {
+      return _this3.getDatasetName($control);
+    });
+    urlParams.delete('layer');
+    enabledLayers.forEach(function (name) {
+      return urlParams.append('layer', name);
+    });
+    console.log(urlParams.toString());
+    var newURL = window.location.pathname + '?' + urlParams.toString() + window.location.hash; // add entry to history, does not fire event so need to call setControls
+
+    window.history.pushState({}, '', newURL);
+    this.setControls();
+  };
+
+  LayerControls.prototype.getCheckbox = function ($control) {
+    return $control.querySelector('input[type="checkbox"]');
+  };
+
+  LayerControls.prototype.enabledLayers = function () {
+    var _this4 = this;
+
+    return this.$controls.filter(function ($control) {
+      return _this4.getCheckbox($control).checked;
+    });
+  };
+
+  LayerControls.prototype.disabledLayers = function () {
+    var _this5 = this;
+
+    return this.$controls.filter(function ($control) {
+      return !_this5.getCheckbox($control).checked;
+    });
+  };
+
+  LayerControls.prototype.getDatasetName = function ($control) {
+    return $control.dataset.layerControl;
+  };
+
+  LayerControls.prototype.getZoomRestriction = function ($control) {
+    return $control.dataset.layerControlZoom;
+  };
+
+  LayerControls.prototype.getStyle = function ($control) {
+    return $control.dataset.layerColour;
+  };
+
+  LayerControls.prototype.defaultOnEachFeature = function (feature, layer) {
+    if (feature.properties) {
+      layer.bindPopup("\n      <h3>".concat(feature.properties.name, "</h3>\n      ").concat(feature.properties.type, "<br>\n      <a href=").concat(this.baseUrl).concat(feature.properties.slug, ">").concat(feature.properties.slug, "</a>\n    "));
+    }
+  };
+
+  LayerControls.prototype.setupOptions = function (params) {
+    params = params || {};
+    this.layerControlSelector = params.layerControlSelector || '[data-layer-control]';
+    this.layerControlDeactivatedClass = params.layerControlDeactivatedClass || 'deactivated-control';
+    this.toggleControlCallback = params.toggleControlCallback || undefined;
+    this.onEachFeature = params.onEachFeature || this.defaultOnEachFeature;
+    this.baseUrl = params.baseUrl || 'http://digital-land.github.io';
+  };
+
+  function ZoomControls($module, leafletMap, initialZoom) {
+    this.$module = $module;
+    this.map = leafletMap;
+    this.initialZoom = initialZoom;
+  }
+
+  ZoomControls.prototype.init = function (params) {
+    this.setupOptions(params);
+
+    if (!this.$module) {
+      return undefined;
+    }
+
+    this.$module.classList.remove('js-hidden');
+    var $buttons = this.$module.querySelectorAll('.' + this.buttonClass);
+    this.$buttons = Array.prototype.slice.call($buttons);
+    this.$counter = this.$module.querySelector(this.counterSelector);
+    this.$counter.textContent = this.initialZoom;
+    var boundClickHandler = this.clickHandler.bind(this);
+    this.$buttons.forEach(function ($button) {
+      $button.addEventListener('click', boundClickHandler);
+    });
+    var boundZoomHandler = this.zoomHandler.bind(this);
+    this.map.addEventListener('zoomend', boundZoomHandler); // get rid of default zoom controls
+
+    this.map.removeControl(this.map.zoomControl);
+    return this;
+  };
+
+  ZoomControls.prototype.clickHandler = function (e) {
+    e.preventDefault();
+    var $clickedEl = e.target;
+    var $clickedControl = $clickedEl; // check if button was pressed
+    // if contained span then find button
+
+    if (!$clickedEl.classList.contains(this.buttonClass)) {
+      $clickedControl = $clickedEl.closest('.' + this.buttonClass);
+    }
+
+    this.zoom($clickedControl.dataset.zoomControl);
+  };
+
+  ZoomControls.prototype.zoom = function (direction) {
+    direction === 'in' ? this.map.zoomIn(1) : this.map.zoomOut(1);
+  };
+
+  ZoomControls.prototype.zoomHandler = function (e) {
+    var zoomLevel = this.map.getZoom();
+    this.$counter.textContent = zoomLevel;
+  };
+
+  ZoomControls.prototype.setupOptions = function (params) {
+    params = params || {};
+    this.buttonClass = params.buttonClass || 'zoom-controls__btn';
+    this.counterSelector = params.counterSelector || '.zoom-controls__count';
+  };
+
   exports.Map = Map;
   exports.brownfieldSites = brownfieldSites;
   exports.basicPopup = basicPopup;
+  exports.LayerControls = LayerControls;
+  exports.ZoomControls = ZoomControls;
 });
